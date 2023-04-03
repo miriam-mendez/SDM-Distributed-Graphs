16c2b9e8dfdacc8477aa5ec577194c2c51a4c229
Miriam Mendez <miriam.mendez.s@gmail.com>
2023-04-03 19:06:42 +0200

    exercise2

--- src/main/java/exercise_2/Exercise_2.java
+++ src/main/java/exercise_2/Exercise_2.java
@@ -26,21 +26,38 @@ public class Exercise_2 {
     private static class VProg extends AbstractFunction3<Long,Integer,Integer,Integer> implements Serializable {
         @Override
         public Integer apply(Long vertexID, Integer vertexValue, Integer message) {
-            return null;
+            if (message == Integer.MAX_VALUE) {             // superstep 0
+                return vertexValue;
+            } else {                                        // superstep > 0
+                return Math.min(vertexValue,message);
+            }
         }
     }
 
     private static class sendMsg extends AbstractFunction1<EdgeTriplet<Integer,Integer>, Iterator<Tuple2<Object,Integer>>> implements Serializable {
         @Override
         public Iterator<Tuple2<Object, Integer>> apply(EdgeTriplet<Integer, Integer> triplet) {
-            return null;
+            Tuple2<Object,Integer> sourceVertex = triplet.toTuple()._1();
+            Tuple2<Object,Integer> dstVertex = triplet.toTuple()._2();
+
+            if (sourceVertex._2 == Integer.MAX_VALUE) {   // source vertex value is smaller than dst vertex?
+                // do nothing
+                return JavaConverters.asScalaIteratorConverter(new ArrayList<Tuple2<Object,Integer>>().iterator()).asScala();
+            } else {
+                // propagate source vertex value
+                return JavaConverters.asScalaIteratorConverter(Arrays.asList(new Tuple2<Object,Integer>(triplet.dstId(),sourceVertex._2 + triplet.toTuple()._3())).iterator()).asScala();
+            }
         }
     }
 
     private static class merge extends AbstractFunction2<Integer,Integer,Integer> implements Serializable {
         @Override
         public Integer apply(Integer o, Integer o2) {
-            return null;
+            if (o >= o2) {
+                return o2;
+            } else {
+                return o;
+            }
         }
     }
 

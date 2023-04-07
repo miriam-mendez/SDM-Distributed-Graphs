82b2499801164d6078bffc2560f46e7bbb95fd7c
Miriam Mendez <miriam.mendez.s@gmail.com>
2023-04-07 05:14:28 +0200

    exercise4 finished

--- src/main/java/exercise_4/Exercise_4.java
+++ src/main/java/exercise_4/Exercise_4.java
@@ -12,15 +12,15 @@ import org.apache.spark.sql.types.MetadataBuilder;
 import org.apache.spark.sql.types.StructField;
 import org.apache.spark.sql.types.StructType;
 import org.graphframes.GraphFrame;
+import static org.apache.spark.sql.functions.*;
 
-import java.util.List;
-import java.io.BufferedReader;
-import java.util.ArrayList;
 
 public class Exercise_4 {
 	static String PATH_EDGES = "/home/miriam/Documentos/SDM/SDM-Distributed-Graphs/src/main/resources/wiki-edges.txt";
 	static String PATH_VERTICES = "/home/miriam/Documentos/SDM/SDM-Distributed-Graphs/src/main/resources/wiki-vertices.txt";
 
+	static double DAMPING_FACTOR = 0.85;
+	static int MAX_ITER = 10;
 
 	public static void wikipedia(JavaSparkContext ctx, SQLContext sqlCtx) {
 		// vertices creation
@@ -51,10 +51,16 @@ public class Exercise_4 {
 		Dataset<Row> edges = sqlCtx.createDataFrame(edges_rdd, edges_schema);
 		
 		GraphFrame gf = GraphFrame.apply(vertices,edges);
-		System.out.println(gf);
+		// System.out.println(gf);
 	
-		gf.edges().show();
-		gf.vertices().show();
+		// gf.edges().show();
+		// gf.vertices().show();
+
+		
+
+		GraphFrame pageRank = gf.pageRank().maxIter(MAX_ITER).resetProbability(1-DAMPING_FACTOR).run();
+		pageRank.vertices().select("id", "title", "pagerank").sort(col("pagerank").desc()).show(10);
+
 
 	}
 	
